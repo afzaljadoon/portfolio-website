2b60bfff4147828d809f3eb8f755b9abfe4f979d
Afzal Khan Jadoon <jadoonafzal027@gmail.com>
2025-03-09 11:10:29 +0500

    Add some functionaity

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/portfolio-website/',
   plugins: [
     tailwindcss(),
   ],
